9d07f8ab2f37f0fbb74787ec93c67616d9d0b4f1
AJMALAJU3 <ajmal.aju.340711@gmail.com>
2024-10-25 10:29:52 +0530

    responsive issue solved

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Wrapper from '../components/Wrapper'
 function Home() {
   return (
-    <section className="grid grid-cols-12 h-screen bg-stone-900">
+    <section className="md:grid md:grid-cols-12 h-screen bg-stone-900">
     <Wrapper />
   </section>
   )
